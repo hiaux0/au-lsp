f29d7893a96d1ff572697cc80555c5aa9a1019d2
hdn <hieudn5293@yahoo.de>
2020-04-12 13:44:31 +0200

    🔧 chore(deps-graph): init

---
+++ _code-dep/index.ts
@@ -0,0 +1,5 @@
+import * as server from '../server/src/server';
+import * as client from '../client/src/extension';
+
+// Command to run (from root dir) to get deps graph
+// ./node_modules/.bin/code-dependency --ex-exclude "node_modules" --source ./_code-dep

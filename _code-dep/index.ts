7c804ec3422579d0ce7aa7c453a836b7936cfb6c
hdn <hieudn5293@yahoo.de>
2020-04-14 23:43:36 +0200

    🔧 chore(package.json): add script for starting depTree server

--- _code-dep/index.ts
+++ _code-dep/index.ts
@@ -3,3 +3,6 @@ import * as client from '../client/src/extension';
 
 // Command to run (from root dir) to get deps graph
 // ./node_modules/.bin/code-dependency --exclude "node_modules" --source ./_code-dep
+// or run
+// `npm run start:depTree`
+// `yarn start:depTree`

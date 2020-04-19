59beae17875a5346140ae310b27a6f97a2731bd6
hdn <hieudn5293@yahoo.de>
2020-04-19 21:59:46 +0200

    feat(program): convenience get and set for projects/program

--- server/src/viewModel/AureliaProgram.ts
+++ server/src/viewModel/AureliaProgram.ts
@@ -1,8 +1,14 @@
-import { singleton } from 'aurelia-dependency-injection';
+import { singleton, inject, autoinject } from 'aurelia-dependency-injection';
 import * as ts from 'typescript';
+import { CompletionItem } from 'vscode-languageserver';
+import { DocumentSettings } from '../configuration/DocumentSettings';
 
 interface IWebcomponent {
 
+}
+
+export interface IComponentMap {
+	classStatements: CompletionItem[] | undefined
 }
 
 /**
@@ -10,9 +16,34 @@ interface IWebcomponent {
  * (aka. program in typescript terminology)
  */
 @singleton()
+@inject(DocumentSettings)
 export class AureliaProgram {
 	public components: IWebcomponent[] = [];
 	public watcherProgram: ts.SemanticDiagnosticsBuilderProgram | undefined;
+	public documentSettings: DocumentSettings;
+
+	private componentMap: IComponentMap;
+
+	constructor(documentSettings: DocumentSettings) {
+		this.documentSettings = documentSettings;
+	}
+
+	public setComponentMap(componentMap: IComponentMap) {
+		this.componentMap = componentMap;
+	}
+
+	public getComponentMap() {
+		return this.componentMap
+	}
+
+	public getProjectFiles() {
+		this.documentSettings
+		const sourceDirectory = ts.sys.getCurrentDirectory();
+		// const paths = ts.sys.readDirectory(sourceDirectory, ['ts', 'js', 'html'], ['node_modules', 'aurelia_project'], ['src']);
+		const paths = ts.sys.readDirectory(sourceDirectory, ['ts', 'js', 'html'], ['node_modules', 'aurelia_project'], ['src']);
+		return paths;
+	}
+
 
   /**
    * getProgram gets the current program

a5362743b0b52113626014f21652072acb80284c
hdn <hieudn5293@yahoo.de>
2020-05-21 19:46:25 +0200

    feat(diagram): assemble string (in mermaid-js md style)

--- server/src/viewModel/createDiagram.ts
+++ server/src/viewModel/createDiagram.ts
@@ -12,13 +12,14 @@ export function createDiagram(classDeclaration: ts.ClassDeclaration, checker: ts
 	let classMethods: ClassInfo[] = [];
 	let classVariables: ClassInfo[] = [];
 
+	// 1. Parse class members (for diagram creation)
 	classDeclaration.forEachChild(classMember => {
 		if (ts.isPropertyDeclaration(classMember) || ts.isMethodDeclaration(classMember)) {
 			const classMemberName = classMember.name?.getText();
 			classMembers.push(classMemberName);
 
 			// Get bindable type. If bindable type is undefined, we set it to be "unknown".
-			const memberType = classMember.type?.getText() !== undefined ? classMember.type?.getText() : "unknown";
+			const memberType = classMember.type?.getText() !== undefined ? classMember.type?.getText() : "";
 
 			// Add comment documentation if available
 			const symbol = checker.getSymbolAtLocation(classMember.name);
@@ -60,7 +61,39 @@ export function createDiagram(classDeclaration: ts.ClassDeclaration, checker: ts
 			}
 		}
 	});
-	classMembers
-	classMethods
-	classVariables
+
+	// 2. Assemble uml string
+	const className = classDeclaration.name?.getText()!;
+	const assembledString = assembleUmlString({ className, classMethods, classVariables });
+}
+
+/**
+ * First iteration: mermaid-js markdown.
+ */
+function assembleUmlString(
+	{ className, classMethods, classVariables }:
+		{ className: string, classMethods: ClassInfo[], classVariables: ClassInfo[] }
+) {
+	const mermaidMdStringStart = '\`\`\`mermaid\n  classDiagram';
+	const classNameString = `class ${className} {`;
+	const mermaidMdStringEnd = '}\n\`\`\`';
+
+	const classVariablesString = classVariables.reduce((acc, classInfo, index) => {
+		const { name, types, documentation } = classInfo
+		return `${acc}
+			${name}: ${types} // ${documentation} [${index}]`;
+	}, '');
+	const classMethodsString = classMethods.reduce((acc, classInfo, index) => {
+		const { name, types, documentation } = classInfo
+		return `${acc}
+      ${name}(): ${types} // ${documentation} [${classVariables.length + index}]`;
+	}, '');
+
+	const result = mermaidMdStringStart + '\n    ' +
+		classNameString + '\n' +
+		classVariablesString + '\n' +
+		classMethodsString + '\n' +
+		mermaidMdStringEnd;
+
+	return result;
 }

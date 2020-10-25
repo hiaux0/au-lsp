7aa2b4217566555a7df99b47ee180bebe932c403
hiaux0 <hieudn5293@yahoo.de>
2020-10-25 13:45:59 +0100

    test(testFixtures): add content to view

--- client/src/test/completion.test.ts
+++ client/src/test/completion.test.ts
@@ -62,7 +62,7 @@ suite.only("Completion", () => {
   test("Should complete class members", async () => {
     await testCompletion(
       docUri,
-      new vscode.Position(1, 23),
+      new vscode.Position(2, 23),
       classMemberTestItems
     );
   });
@@ -70,7 +70,7 @@ suite.only("Completion", () => {
   test("Should complete class members - bindables", async () => {
     await testCompletion(
       docUri,
-      new vscode.Position(1, 23),
+      new vscode.Position(2, 23),
       bindablesTestItems
     );
   });

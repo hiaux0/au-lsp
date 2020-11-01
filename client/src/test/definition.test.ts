0f3a3e98e984a5cd78296ed1f81e85757fdbdbfb
hiaux0 <hieudn5293@yahoo.de>
2020-11-01 16:43:02 +0100

    test(definition): more tests

--- client/src/test/definition.test.ts
+++ client/src/test/definition.test.ts
@@ -15,27 +15,51 @@ import * as assert from "assert";
 
 import { activate, getTestApplicationFiles } from "./helper";
 
-suite("Definition", () => {
+suite.only("Definition", () => {
   const applicationFile = getTestApplicationFiles();
   const docUri = vscode.Uri.file(applicationFile.viewPaths[0]);
 
   test("Complete class declaration", async () => {
-    // <!-- Test: Go to definition [METHOD] -->
-    await testCompletion(docUri, new vscode.Position(5, 30), {
-      position: new vscode.Position(10, 2),
+    // <!-- Test: Go to definition [METHOD] {{ISSUE-15pHmj6C}}-->
+    await testCompletion(docUri, new vscode.Position(7, 23), {
+      position: new vscode.Position(24, 2),
       partOfFilePath: "testFixture/src/compo-user/compo-user.ts",
     });
 
-    // <!-- Test: Go to definition [VARIABLE] -->
-    await testCompletion(docUri, new vscode.Position(8, 21), {
-      position: new vscode.Position(8, 2),
+    // <!-- Test: Go to definition [VARIABLE] {{ISSUE-oITTCXJc}}-->
+    await testCompletion(docUri, new vscode.Position(11, 21), {
+      position: new vscode.Position(16, 2),
       partOfFilePath: "testFixture/src/compo-user/compo-user.ts",
     });
 
-    // <!-- Test: Go to definition [CUSTOM_ELEMENT] -->
-    await testCompletion(docUri, new vscode.Position(11, 3), {
+    // <!-- Test: Go to definition [CUSTOM_ELEMENT] {{ISSUE-lj6q5QtN}}-->
+    await testCompletion(docUri, new vscode.Position(15, 3), {
       position: new vscode.Position(1, 1),
       partOfFilePath: "testFixture/src/my-compo/my-compo.ts",
+    });
+
+    // <!-- Test: Go to definition [CUSTOM_ELEMENT_BINDABLE] {{ISSUE-8Q6EL3Ui}}-->
+    await testCompletion(docUri, new vscode.Position(20, 5), {
+      position: new vscode.Position(27, 2),
+      partOfFilePath: "testFixture/src/my-compo/my-compo.ts",
+    });
+
+    // <!-- Test: Definition - Repeat for {{ISSUE-5ugNzvtm}} -->
+    await testCompletion(docUri, new vscode.Position(33, 8), {
+      position: new vscode.Position(30, 23),
+      partOfFilePath: "testFixture/src/compo-user/compo-user.html",
+    });
+
+    // <!-- Text interpolated region {{ISSUE-sCxw9bfm}}-->
+    await testCompletion(docUri, new vscode.Position(27, 8), {
+      position: new vscode.Position(20, 2),
+      partOfFilePath: "testFixture/src/compo-user/compo-user.ts",
+    });
+
+    // <!-- Text interpolated repeat-for region {{ISSUE-uMZ1grJD}}-->
+    await testCompletion(docUri, new vscode.Position(30, 31), {
+      position: new vscode.Position(20, 2),
+      partOfFilePath: "testFixture/src/compo-user/compo-user.ts",
     });
   });
 });

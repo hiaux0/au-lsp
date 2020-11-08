579f1112c3acf1bd6e094d99a11c0f9bc8216c52
hiaux0 <hieudn5293@yahoo.de>
2020-11-08 11:50:54 +0100

    test(valueConverter): add case

---
+++ client/testFixture/src/value-converters/sort-value-converter.ts
@@ -0,0 +1,14 @@
+class SortError extends Error {
+  constructor() {
+    super();
+  }
+}
+
+export class SortValueConverter {
+  toView(array, config) {
+    let factor = (config.direction || "ascending") === "ascending" ? 1 : -1;
+    return array.sort((a, b) => {
+      return (a[config.propertyName] - b[config.propertyName]) * factor;
+    });
+  }
+}

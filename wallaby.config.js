d7c5b152760d160c3fe098ec45db54bb0c15cd10
hiaux0 <hieudn5293@yahoo.de>
2021-02-19 15:53:17 +0100

    dev(wallaby)

--- wallaby.config.js
+++ wallaby.config.js
@@ -3,10 +3,12 @@ module.exports = function () {
 	  files: [
 		'client/**/*.ts',
 		'server/**/*.ts',
+		'**/tsconfig.json'
 	  ],
 
 	  tests: [
-		'tests/unit/**/*.spec.ts',
+		// 'tests/unit/**/*.spec.ts',
+		'tests/unit/**/*Map.spec.ts',
 	  ],
 
 	  testFramework: 'mocha',

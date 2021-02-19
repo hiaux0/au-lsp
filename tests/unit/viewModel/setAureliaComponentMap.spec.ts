db879c8fb38d68355c02841b403a5d561745debc
hiaux0 <hieudn5293@yahoo.de>
2021-02-19 16:48:59 +0100

    test(viewModel): bindables check

--- tests/unit/viewModel/setAureliaComponentMap.spec.ts
+++ tests/unit/viewModel/setAureliaComponentMap.spec.ts
@@ -1,10 +1,10 @@
 import "reflect-metadata";
 import { Container } from "aurelia-dependency-injection";
-import { strictEqual } from "assert";
-import * as path from 'path';
+import { equal, ok, strictEqual } from "assert";
+import { expect } from "chai";
+import * as path from "path";
 
 import { AureliaProgram } from "../../../server/src/viewModel/AureliaProgram";
-import { setAureliaComponentMap } from "../../../server/src/viewModel/setAureliaComponentMap";
 import { createAureliaWatchProgram } from "../../../server/src/viewModel/createAureliaWatchProgram";
 
 // const testAureliaProgram = new AureliaProgram();
@@ -12,9 +12,15 @@ import { createAureliaWatchProgram } from "../../../server/src/viewModel/createA
 export function getAureliaProgramForTesting() {
   const container = new Container();
   const aureliaProgram = container.get(AureliaProgram);
-    const sourceDirectory = path.resolve(__dirname, "../../../client/testFixture");
+  const sourceDirectory = path.resolve(
+    __dirname,
+    "../../../client/testFixture"
+  );
 
-  createAureliaWatchProgram(aureliaProgram, sourceDirectory);
+  createAureliaWatchProgram(aureliaProgram, {
+    sourceDirectory,
+    include: ["src/realdworld-advanced"],
+  });
   return aureliaProgram;
 }
 
@@ -25,8 +31,8 @@ describe("Aurelia Component Map", () => {
   });
 
   it("setAureliaComponentMap", () => {
-    const asht = testAureliaProgram.getComponentMap();
-    asht /*?*/
-    strictEqual(asht, { hi: "" });
+    const componentMap = testAureliaProgram.getComponentMap();
+    console.log('TCL: componentMap.bindables', componentMap.bindables)
+    strictEqual(componentMap.bindables.length, 0);
   });
 });

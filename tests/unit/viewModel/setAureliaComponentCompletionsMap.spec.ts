4e2b033b47017f83ebc989d00588f6910362bdb4
hiaux0 <hieudn5293@yahoo.de>
2021-02-21 13:50:38 +0100

    lint(test): viewModel

--- tests/unit/viewModel/setAureliaComponentCompletionsMap.spec.ts
+++ tests/unit/viewModel/setAureliaComponentCompletionsMap.spec.ts
@@ -1,31 +1,16 @@
 import 'reflect-metadata';
-import { Container } from 'aurelia-dependency-injection';
 import { strictEqual } from 'assert';
-import * as path from 'path';
 
 import { AureliaProgram } from '../../../server/src/viewModel/AureliaProgram';
-import { createAureliaWatchProgram } from '../../../server/src/viewModel/createAureliaWatchProgram';
 import { CompletionItemKind } from 'vscode-html-languageservice';
-
-export function getAureliaProgramForTesting(include: string[]): AureliaProgram {
-  const container = new Container();
-  const aureliaProgram = container.get(AureliaProgram);
-  const sourceDirectory = path.resolve(
-    __dirname,
-    '../../../client/testFixture'
-  );
-
-  createAureliaWatchProgram(aureliaProgram, {
-    sourceDirectory,
-    include,
-  });
-  return aureliaProgram;
-}
+import { getAureliaProgramForTesting } from '../helpers/test-setup';
 
 let testAureliaProgram: AureliaProgram;
 describe('Aurelia Component Map', () => {
   before(() => {
-    testAureliaProgram = getAureliaProgramForTesting(['src/realdworld-advanced']);
+    testAureliaProgram = getAureliaProgramForTesting([
+      'src/realdworld-advanced',
+    ]);
   });
 
   it('setAureliaComponentCompletionsMap', () => {
@@ -33,14 +18,21 @@ describe('Aurelia Component Map', () => {
     strictEqual(componentCompletionsMap.bindables?.length, 0);
 
     strictEqual(componentCompletionsMap.classDeclarations?.length, 1);
-    strictEqual(componentCompletionsMap.classDeclarations[0].kind, CompletionItemKind.Class);
+    strictEqual(
+      componentCompletionsMap.classDeclarations[0].kind,
+      CompletionItemKind.Class
+    );
 
     strictEqual(componentCompletionsMap.classMembers?.length, 6);
 
-    const variables = componentCompletionsMap.classMembers.filter(classMember => classMember.kind === CompletionItemKind.Field);
+    const variables = componentCompletionsMap.classMembers.filter(
+      (classMember) => classMember.kind === CompletionItemKind.Field
+    );
     strictEqual(variables?.length, 2);
 
-    const methods = componentCompletionsMap.classMembers.filter(classMember => classMember.kind === CompletionItemKind.Method);
+    const methods = componentCompletionsMap.classMembers.filter(
+      (classMember) => classMember.kind === CompletionItemKind.Method
+    );
     strictEqual(methods?.length, 4);
   });
 });

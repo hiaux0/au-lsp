8f4872a165bda503a1835e632d380ffc83b53033
hiaux0 <hieudn5293@yahoo.de>
2021-02-24 18:04:20 +0100

    test(helpers): add test document creation

--- tests/unit/helpers/test-setup.ts
+++ tests/unit/helpers/test-setup.ts
@@ -1,9 +1,14 @@
-import { defaultProjectOptions , IProjectOptions } from '../../../server/src/common/common.types';
+import {
+  defaultProjectOptions,
+  IProjectOptions,
+} from '../../../server/src/common/common.types';
 import { Container } from 'aurelia-dependency-injection';
 import * as path from 'path';
+import * as fs from 'fs';
 
 import { AureliaProgram } from '../../../server/src/viewModel/AureliaProgram';
 import { createAureliaWatchProgram } from '../../../server/src/viewModel/createAureliaWatchProgram';
+import { TextDocument } from 'vscode-html-languageservice';
 
 export function getAureliaProgramForTesting(
   projectOptions: IProjectOptions = defaultProjectOptions
@@ -19,4 +24,11 @@ export function getAureliaProgramForTesting(
 
   createAureliaWatchProgram(aureliaProgram, projectOptions);
   return aureliaProgram;
+}
+
+export function createTextDocumentForTesting(filePath: string): TextDocument {
+  const uri = filePath;
+  const content = fs.readFileSync(uri, 'utf-8');
+  const document = TextDocument.create(uri, 'html', 99, content);
+  return document;
 }

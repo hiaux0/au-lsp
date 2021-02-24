ed395623f5b2c73d15f7c7df09b7a226932ca960
hiaux0 <hieudn5293@yahoo.de>
2021-02-24 18:04:29 +0100

    lint(wallaby)

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,24 +1,25 @@
 module.exports = function () {
-	return {
-	  files: [
-		'client/**/*.ts',
-		'client/testFixture/src/**/*.html',
-		'server/**/*.ts',
-		'**/tsconfig.json',
-		'tests/unit/helpers/**/*.ts'
-	  ],
+  return {
+    files: [
+      "client/**/*.ts",
+      "client/testFixture/src/**/*.html",
+      "server/**/*.ts",
+      "**/tsconfig.json",
+      "tests/unit/helpers/**/*.ts",
+    ],
 
-	  tests: [
-		// 'tests/unit/**/*.spec.ts',
-		// 'tests/unit/**/*Map.spec.ts',
-		'tests/unit/**/*embeddedSupport.spec.ts',
-		// 'tests/unit/**/AureliaProgram.spec.ts',
-	  ],
+    tests: [
+      // 'tests/unit/**/*.spec.ts',
+      // 'tests/unit/**/*Map.spec.ts',
+      // 'tests/unit/**/*embeddedSupport.spec.ts',
+      "tests/unit/**/languageModes.spec.ts",
+      // 'tests/unit/**/AureliaProgram.spec.ts',
+    ],
 
-	  testFramework: 'mocha',
-	  env: {
-		type: 'node'
-	  },
-	  debug: true
-	};
+    testFramework: "mocha",
+    env: {
+      type: "node",
+    },
+    debug: true,
   };
+};

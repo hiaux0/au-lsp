137e561446b121a20fa07daf70f6efb16d86b0d0
hiaux0 <hieudn5293@yahoo.de>
2021-02-27 20:31:00 +0100

    test(embedded.completions): init

--- tests/unit/helpers/test-setup.ts
+++ tests/unit/helpers/test-setup.ts
@@ -6,20 +6,22 @@ import * as path from 'path';
 import * as fs from 'fs';
 import 'reflect-metadata';
 
-import { AureliaProgram, globalContainer } from '../../../server/src/viewModel/AureliaProgram';
+import {
+  AureliaProgram,
+  globalContainer,
+} from '../../../server/src/viewModel/AureliaProgram';
 import { createAureliaWatchProgram } from '../../../server/src/viewModel/createAureliaWatchProgram';
-import { TextDocument } from 'vscode-html-languageservice';
+import { Position, TextDocument } from 'vscode-html-languageservice';
 import { Container } from 'aurelia-dependency-injection';
+import { AureliaCompletionItem, isAureliaCompletionItem } from '../../../server/src/feature/completions/virtualCompletion';
+import { createTextDocumentPositionParams, getLanguageModes } from '../../../server/src/feature/embeddedLanguages/languageModes';
 
 export function getAureliaProgramForTesting(
   projectOptions: IProjectOptions = defaultProjectOptions
 ): AureliaProgram {
   const container: Container = globalContainer;
   const aureliaProgram = container.get(AureliaProgram);
-  const sourceDirectory = path.resolve(
-    __dirname,
-    '../../testFixture'
-  );
+  const sourceDirectory = path.resolve(__dirname, '../../testFixture');
 
   projectOptions.sourceDirectory = sourceDirectory;
 
@@ -27,9 +29,55 @@ export function getAureliaProgramForTesting(
   return aureliaProgram;
 }
 
-export function createTextDocumentForTesting(filePath: string): TextDocument {
+export function createTextDocumentForTesting(
+  filePath: string,
+  content?: string
+): TextDocument {
   const uri = filePath;
-  const content = fs.readFileSync(uri, 'utf-8');
+  content = content ?? fs.readFileSync(uri, 'utf-8');
   const document = TextDocument.create(uri, 'html', 99, content);
   return document;
+}
+
+export class TestSetup {
+  public static async createCompletionTest(
+    aureliaProgram: AureliaProgram,
+    options: {
+      templatePath: string;
+      templateContent: string;
+      position: Position;
+      triggerCharacter?: string;
+    }
+  ): Promise<AureliaCompletionItem[]> {
+    /** Indicate, that aureliaProgram is needed. */
+    void aureliaProgram;
+
+    const { templatePath, templateContent, position, triggerCharacter } = options;
+
+    const testPath = path.resolve(__dirname, '../../testFixture');
+    const targetViewPath = path.resolve(testPath, templatePath);
+    const languageModes = await getLanguageModes();
+    const document = createTextDocumentForTesting(targetViewPath, templateContent);
+    const modeAndRegion = await languageModes.getModeAndRegionAtPosition(
+      document,
+      position
+    );
+    const textDocument = createTextDocumentPositionParams(document, position);
+
+    if (modeAndRegion?.mode?.doComplete === undefined) {
+      throw new Error('doComplete not provded.');
+    }
+
+    const completion = await modeAndRegion?.mode?.doComplete(
+      document,
+      textDocument,
+      triggerCharacter ?? '',
+    );
+
+    if (!isAureliaCompletionItem(completion)) {
+      throw new Error('Not AureliaCompletionItem[]');
+    }
+
+    return completion;
+  }
 }

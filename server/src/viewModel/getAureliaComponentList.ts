34896451169fcd183196f867cd1bf4d8f71553c0
hiaux0 <hieudn5293@yahoo.de>
2021-02-28 20:06:46 +0100

    move(viewModel.auc): (5/5) function and delete obsolete

--- server/src/viewModel/getAureliaComponentList.ts
+++ server/src/viewModel/getAureliaComponentList.ts
@@ -19,13 +19,7 @@ import { IAureliaClassMember, IAureliaComponent } from './AureliaProgram';
 import * as ts from 'typescript';
 import * as Path from 'path';
 import { getElementNameFromClassDeclaration } from '../common/className';
-import { AureliaClassTypes, VALUE_CONVERTER_SUFFIX } from '../common/constants';
-import {
-  classDeclarationHasUseViewOrNoView,
-  getTemplateImportPathFromCustomElementDecorator,
-  hasCustomElementNamingConvention,
-  hasValueConverterNamingConvention,
-} from './setAureliaComponentCompletionsMap';
+import { AureliaClassTypes, AureliaDecorator, AureliaViewModel, VALUE_CONVERTER_SUFFIX } from '../common/constants';
 
 export function getAureliaComponentInfoFromClassDeclaration(
   sourceFile: ts.SourceFile,
@@ -201,4 +195,127 @@ export function getAureliaViewModelClassMembers(
   });
 
   return classMembers;
+}
+
+/**
+ * classDeclarationHasUseViewOrNoView checks whether a classDeclaration has a useView or noView
+ *
+ * @param classDeclaration - ClassDeclaration to check
+ */
+export function classDeclarationHasUseViewOrNoView(
+  classDeclaration: ts.ClassDeclaration
+): boolean {
+  if (!classDeclaration.decorators) return false;
+
+  const hasViewDecorator = classDeclaration.decorators.some((decorator) => {
+    const result =
+      decorator.getText().includes('@useView') ||
+      decorator.getText().includes('@noView');
+    return result;
+  });
+
+  return hasViewDecorator;
+}
+
+/**
+ * MyClassCustomelement
+ *
+ * \@customElement(...)
+ * MyClass
+ */
+export function hasCustomElementNamingConvention(
+  classDeclaration: ts.ClassDeclaration
+): boolean {
+  const hasCustomElementDecorator =
+    classDeclaration.decorators?.some((decorator) => {
+      const result = decorator
+        .getText()
+        .includes(AureliaDecorator.CUSTOM_ELEMENT);
+      return result;
+    }) ?? false;
+
+  const hasCustomElementNamingConvention = Boolean(
+    classDeclaration.name?.getText().includes(AureliaClassTypes.CUSTOM_ELEMENT)
+  );
+
+  return hasCustomElementDecorator || hasCustomElementNamingConvention;
+}
+
+/**
+ * MyClassValueConverter
+ *
+ * \@valueConverter(...)
+ * MyClass
+ */
+export function hasValueConverterNamingConvention(
+  classDeclaration: ts.ClassDeclaration
+): boolean {
+  const hasValueConverterDecorator =
+    classDeclaration.decorators?.some((decorator) => {
+      const result = decorator
+        .getText()
+        .includes(AureliaDecorator.VALUE_CONVERTER);
+      return result;
+    }) ?? false;
+
+  const hasValueConverterNamingConvention = Boolean(
+    classDeclaration.name?.getText().includes(AureliaClassTypes.VALUE_CONVERTER)
+  );
+
+  return hasValueConverterDecorator || hasValueConverterNamingConvention;
+}
+
+export function getTemplateImportPathFromCustomElementDecorator(
+  classDeclaration: ts.ClassDeclaration,
+  sourceFile: ts.SourceFile
+): string | undefined {
+  if (!classDeclaration.decorators) return;
+
+  const customElementDecorator = classDeclaration.decorators.find(
+    (decorator) => {
+      const result = decorator
+        .getText()
+        .includes(AureliaDecorator.CUSTOM_ELEMENT);
+      return result;
+    }
+  );
+
+  if (!customElementDecorator) return;
+
+  const hasTemplateProp = customElementDecorator
+    .getText()
+    .includes(AureliaViewModel.TEMPLATE);
+  if (!hasTemplateProp) return;
+
+  let templateImportPath = '';
+  const templateImport = sourceFile.statements.find((statement) => {
+    const isImport = statement.kind === ts.SyntaxKind.ImportDeclaration;
+    if (!isImport) {
+      return false;
+    }
+
+    let foundTemplateImport = false;
+    statement.getChildren().forEach((child) => {
+      if (child.kind === ts.SyntaxKind.ImportClause) {
+        if (child.getText().includes(AureliaViewModel.TEMPLATE)) {
+          foundTemplateImport = true;
+        }
+      }
+    });
+
+    return foundTemplateImport;
+  });
+
+  templateImport?.getChildren().forEach((child) => {
+    if (child.kind === ts.SyntaxKind.StringLiteral) {
+      templateImportPath = child.getText().replace(/['"]/g, '');
+    }
+  });
+
+  templateImportPath = Path.resolve(
+    Path.dirname(sourceFile.fileName),
+    templateImportPath
+  );
+
+  return templateImportPath;
 }
